00a7048dc896fec9c235ecb0a4de630d02f570ea
brunoamaia <brunoamaia94@gmail.com>
2020-09-29 16:30:13 -0300

    ajuste nas contas

--- production/script.js
+++ production/script.js
@@ -3,27 +3,27 @@ let textuse = window.document.querySelector(".useproductiontext")
 let progressbar = document.querySelector("#useprogress")
 
 function increment() {
-  if (use < 100) {
+  let temp = use + 5
+  if (temp <= 100){
     use +=5;
-    if (use <= 100) {
-      textuse.innerHTML = `${use}`
-      progressbar.value = use
-    } else {
-      textuse.innerHTML = `100`
-      progressbar.value = use
-    }
+    textuse.innerHTML = `${use}%`
+    progressbar.value = use
+  } else {
+    use = 100
+    textuse.innerHTML = `${use}%`
+    progressbar.value = use
   }
 }
 
 function decrement() {
-  if (use > 0){
+  let temp = use - 5
+  if (temp > 0){
     use -=5;
-    if (use >= 0) {
-      textuse.innerHTML = `${use}`
-      progressbar.value = use
-    } else {
-      textuse.innerHTML = `0`
-      progressbar.value = use
-    }
+    textuse.innerHTML = `${use}%`
+    progressbar.value = use
+  } else {
+    use = 1
+    textuse.innerHTML = `${use}%`
+    progressbar.value = use
   }
 }

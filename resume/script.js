8287e6d05a81bd70f0c99f05563dcbe480bfed2e
brunoamaia <brunoamaia94@gmail.com>
2020-09-29 17:29:29 -0300

    ajuste da barra de progresso de pedidos

--- resume/script.js
+++ resume/script.js
@@ -1,29 +1,89 @@
-let use = 80
+let production = 80
 let textuse = window.document.querySelector(".useproductiontext")
-let progressbar = document.querySelector("#useprogress")
-
-function increment() {
-  let temp = use + 5
-  if (temp <= 100){
-    use +=5;
-    textuse.innerHTML = `${use}%`
-    progressbar.value = use
+let progressbar = document.querySelector("#useproduction")
+function decrementproduction() {
+  let temp = production - 5
+  if (temp > 0){
+    production -=5;
+    textuse.innerHTML = `${production.toFixed(2)}%`
+    progressbar.value = production
   } else {
-    use = 100
-    textuse.innerHTML = `${use}%`
-    progressbar.value = use
+    production = 1
+    textuse.innerHTML = `${production.toFixed(2)}%`
+    progressbar.value = production
+  }
+}
+function incrementproduction() {
+  let temp = production + 5
+  if (temp <= 100){
+    production +=5;
+    textuse.innerHTML = `${production.toFixed(2)}%`
+    progressbar.value = production
+  } else {
+    production = 100
+    textuse.innerHTML = `${production.toFixed(2)}%`
+    progressbar.value = production
   }
 }
 
-function decrement() {
-  let temp = use - 5
+
+
+
+let textorder = window.document.querySelector(".doingorder")
+let progressorder = document.querySelector("#useorder")
+let order = 20
+let ordertotal = 50
+let jumpOrder = 5
+function decrementorder() {
+  let temp = order - jumpOrder
   if (temp > 0){
-    use -=5;
-    textuse.innerHTML = `${use}%`
-    progressbar.value = use
+    order -=jumpOrder;
+    textorder.innerHTML = `${order} => ${(order*100/ordertotal).toFixed(2)}%`
+    progressorder.value = order*100/ordertotal
   } else {
-    use = 1
-    textuse.innerHTML = `${use}%`
-    progressbar.value = use
+    order = 1
+    textorder.innerHTML = `${order} => ${(order*100/ordertotal).toFixed(2)}%`
+    progressorder.value = order*100/ordertotal
+  }
+}
+function incrementorder() {
+  let temp = order + jumpOrder
+  if (temp <= ordertotal){
+    order +=jumpOrder;
+    textorder.innerHTML = `${order} => ${(order*100/ordertotal).toFixed(2)}%`
+    progressorder.value = order*100/ordertotal
+  } else {
+    order = ordertotal
+    textorder.innerHTML = `${order} => ${(order*100/ordertotal).toFixed(2)}%`
+    progressorder.value = order*100/ordertotal
+  }
+}
+
+
+let hwe = 80
+let text = window.document.querySelector(".")
+let progress = document.querySelector("#")
+function decrement() {
+  let temp = hwe - 5
+  if (temp > 0){
+    hwe -=5;
+    text.innerHTML = `${hwe}%`
+    progress.value = hwe
+  } else {
+    hwe = 1
+    text.innerHTML = `${hwe}%`
+    progress.value = hwe
+  }
+}
+function increment() {
+  let temp = hwe + 5
+  if (temp <= 100){
+    hwe +=5;
+    text.innerHTML = `${hwe}%`
+    progress.value = hwe
+  } else {
+    hwe = 100
+    text.innerHTML = `${hwe}%`
+    progress.value = hwe
   }
 }

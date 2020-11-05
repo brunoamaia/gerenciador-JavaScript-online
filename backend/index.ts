a3b23a9edf032c7240c2687a3bed04a374d1d599
brunoamaia <brunoamaia94@gmail.com>
2020-11-05 15:55:39 -0300

    mudar estrutura

--- backend/index.ts
+++ backend/index.ts
@@ -3,4 +3,6 @@ import express from 'express';
 const app = express();
 app.use(express.json());  // para o node "entender" json
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Backend executando...')
+});

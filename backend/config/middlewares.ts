d9fbb4d45283bb3d9e82451c12e336ffe66b480d
brunoamaia <brunoamaia94@gmail.com>
2020-11-09 16:38:18 -0300

    adicionar middlewares

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,1 +1,7 @@
 import bodyParser from 'body-parser'
+import cors from 'cors';
+
+module.exports = app => {
+  app.use(bodyParser.json())
+  app.use(cors())
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+const app = require('express')();
+const consign = require('consign');
 
-const app = express();
-app.use(express.json());  // para o node "entender" json
+consign()
+  .then('./config/middlewares.ts')
+  .into(app)
+
 
 app.listen(3333, () => {
   console.log('Backend executando...')

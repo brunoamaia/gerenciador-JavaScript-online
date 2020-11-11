5f9ca180dc05b3be8667baa3ae5a0e1d0e6ad7ce
brunoamaia <brunoamaia94@gmail.com>
2020-11-11 15:34:05 -0300

    Criação das migrations

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const config = require('../knexfile')
 const knex = require('knex')(config)
 
-knex.migrate.latest([config])  
+knex.migrate.latest([config])  // sempre executar todas as migrations antes de ligar o backend
 module.exports = knex

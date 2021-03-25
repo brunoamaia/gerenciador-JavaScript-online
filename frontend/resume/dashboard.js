a84c750daac75f669cd6c23ad4a40b5106b19263
brunoamaia <brunoamaia94@gmail.com>
2021-03-25 16:11:51 -0300

    insert line of points

--- frontend/resume/dashboard.js
+++ frontend/resume/dashboard.js
@@ -9,21 +9,34 @@ const inventory = data.inventory.production
 const supply = data.inventory.supply
 
 function resized() {
-  drawGraphicOrder()
   dimensions.width = window.innerWidth * 0.9
+  drawGraphicOrder()
+  console.log('lol')
 }
-
-
 
 const dimensions = {
   height: 250,
   width: window.innerWidth * 0.9
 }
+const xmin = dimensions.width * 0.1
+const xmax = dimensions.width * 0.95
+const ymin = dimensions.height * 0.85
+const ymax = dimensions.height * 0.1
+
+function xnormalized(position) {
+  let value = xmin + (position * (xmax-xmin))/orderData.length
+  return value
+} 
+
+function ynormalized(position) {
+  let value = ymin - (orderData[position] * (ymin-ymax))/Math.max(...orderData)
+  return value 
+}
 
 var graphicOrder = document.getElementById("graphic-order");
 var context = graphicOrder.getContext("2d");
 
-function resizeScreen() {
+function resizeGraphic() {
   graphicOrder.height = dimensions.height
   graphicOrder.width = dimensions.width
 }
@@ -31,30 +44,38 @@ function resizeScreen() {
 function xAxis() {
   context.strokeStyle = '#FAFAFA'
   context.beginPath();
-  context.moveTo(15, dimensions.height * 0.85);
-  context.lineTo(dimensions.width * 0.95, dimensions.height * 0.85);
+  context.moveTo(xmin, ymin);
+  context.lineTo(xmax, ymin);
   context.stroke();
 }
 
 function yAxis() {
   context.strokeStyle = '#000'
   context.beginPath();
-  context.moveTo(30, 15);
-  context.lineTo(30, dimensions.height * 0.9);
+  context.moveTo(xmin, ymin);
+  context.lineTo(xmin, ymax);
   context.stroke();
 }
 
+function drawLine() {
+  context.strokeStyle = '#EFB92E'
+
+  for (let i = 0; i<month.length-1; i++) {
+    context.beginPath();
+    context.moveTo(xnormalized(i), ynormalized(i));
+    context.lineTo(xnormalized(i+1), ynormalized(i+1));
+    context.stroke();
+  }
+}
 
 function drawGraphicOrder() {
-  resizeScreen()
+  resizeGraphic()
 
   xAxis()
   yAxis()
+  drawLine()
 
-
-  setTimeout(drawGraphicOrder, 10)
+  setTimeout(drawGraphicOrder, 100)
 }
 
 drawGraphicOrder()
-
-// const data = require('../data.json')

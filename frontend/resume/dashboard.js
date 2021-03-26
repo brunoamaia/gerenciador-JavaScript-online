84eb41a3f54a7a2170f06198e5ed11acbaafd7b7
brunoamaia <brunoamaia94@gmail.com>
2021-03-26 11:14:06 -0300

    insert x label

--- frontend/resume/dashboard.js
+++ frontend/resume/dashboard.js
@@ -19,7 +19,7 @@ const dimensions = {
 }
 
 const graphArea = {
-  xini: dimensions.width  * 0.03,
+  xini: dimensions.width  * 0.001,
   xmin: dimensions.width  * 0.1,
   xmax: dimensions.width  * 0.95,
   yini: dimensions.height * 0.98,
@@ -41,7 +41,7 @@ var graphicOrder = document.getElementById("graphic-order");
 var context = graphicOrder.getContext("2d");
 
 function updateSizeGraph() {
-  graphArea.xini = dimensions.width  * 0.03,
+  graphArea.xini = dimensions.width  * 0.001,
   graphArea.xmin = dimensions.width * 0.1
   graphArea.xmax = dimensions.width * 0.95
   graphArea.yini = dimensions.height * 0.98
@@ -65,11 +65,43 @@ function xAxis() {
   context.stroke();
 }
 
+function xlabel(array, lines) {
+  context.fillStyle = '#FAFAFA'
+  context.textAlign = "start"
+  context.font = '14px Nunito';
+
+  let max = Math.max(...array)
+  let residue = max%5;
+  if (residue !== 0) {
+    max += residue
+  }
+
+  let jump = max/lines
+  let label = []
+  let isfloat = false
+  for (let i = 0; i <= lines; i++) {
+    label[i] = jump*i
+    if (label[i] % 1 !== 0) {
+      isfloat = true
+    }
+  }
+
+  let maxWidth = (graphArea.xmin - graphArea.xini)*0.92
+  let yjump = (graphArea.ymin - graphArea.ymax)/lines
+
+  for (let i = 0; i <= lines; i++) {
+    let value = (isfloat) ? label[i-1].toFixed(2) : label[i]
+    context.fillText(`$ ${value}`, graphArea.xini, (graphArea.ymin-yjump*i)+7, maxWidth);
+  }
+
+}
+
+
 function yAxis() {
   context.strokeStyle = '#000'
   context.beginPath();
   context.moveTo(graphArea.xmin, graphArea.ymin);
-  context.lineTo(graphArea.xmin, graphArea.ymax);
+  context.lineTo(graphArea.xmin, graphArea.ymax-14);
   context.stroke();
 }
 
@@ -88,10 +120,11 @@ function drawGraphicOrder() {
   resizeGraphic()
 
   xAxis()
+  xlabel(orderData, 5)
   yAxis()
   drawLine(orderData)
 
-  setTimeout(drawGraphicOrder, 300)
+  setTimeout(drawGraphicOrder, 3000)
 }
 
 drawGraphicOrder()

00c53c4fb640f677ed608a4408ff312440bbc29f
brunoamaia <brunoamaia94@gmail.com>
2021-03-26 09:59:17 -0300

    Auto resize graph

--- frontend/resume/dashboard.js
+++ frontend/resume/dashboard.js
@@ -10,31 +10,47 @@ const supply = data.inventory.supply
 
 function resized() {
   dimensions.width = window.innerWidth * 0.9
-  drawGraphicOrder()
-  console.log('lol')
+  updateSizeGraph()
 }
 
 const dimensions = {
   height: 250,
   width: window.innerWidth * 0.9
 }
-const xmin = dimensions.width * 0.1
-const xmax = dimensions.width * 0.95
-const ymin = dimensions.height * 0.85
-const ymax = dimensions.height * 0.1
+
+const graphArea = {
+  xini: dimensions.width  * 0.03,
+  xmin: dimensions.width  * 0.1,
+  xmax: dimensions.width  * 0.95,
+  yini: dimensions.height * 0.98,
+  ymin: dimensions.height * 0.85,
+  ymax: dimensions.height * 0.1
+}
 
 function xnormalized(position) {
-  let value = xmin + (position * (xmax-xmin))/orderData.length
+  let value = graphArea.xmin + (position * (graphArea.xmax-graphArea.xmin))/(orderData.length-1)
   return value
 } 
 
 function ynormalized(position) {
-  let value = ymin - (orderData[position] * (ymin-ymax))/Math.max(...orderData)
+  let value = graphArea.ymin - (orderData[position] * (graphArea.ymin-graphArea.ymax))/Math.max(...orderData)
   return value 
 }
 
 var graphicOrder = document.getElementById("graphic-order");
 var context = graphicOrder.getContext("2d");
+
+function updateSizeGraph() {
+  graphArea.xini = dimensions.width  * 0.03,
+  graphArea.xmin = dimensions.width * 0.1
+  graphArea.xmax = dimensions.width * 0.95
+  graphArea.yini = dimensions.height * 0.98
+  graphArea.ymin = dimensions.height * 0.85
+  graphArea.ymax = dimensions.height * 0.1
+
+  drawGraphicOrder()
+}
+
 
 function resizeGraphic() {
   graphicOrder.height = dimensions.height
@@ -44,16 +60,16 @@ function resizeGraphic() {
 function xAxis() {
   context.strokeStyle = '#FAFAFA'
   context.beginPath();
-  context.moveTo(xmin, ymin);
-  context.lineTo(xmax, ymin);
+  context.moveTo(graphArea.xmin, graphArea.ymin);
+  context.lineTo(graphArea.xmax, graphArea.ymin);
   context.stroke();
 }
 
 function yAxis() {
   context.strokeStyle = '#000'
   context.beginPath();
-  context.moveTo(xmin, ymin);
-  context.lineTo(xmin, ymax);
+  context.moveTo(graphArea.xmin, graphArea.ymin);
+  context.lineTo(graphArea.xmin, graphArea.ymax);
   context.stroke();
 }
 
@@ -75,7 +91,7 @@ function drawGraphicOrder() {
   yAxis()
   drawLine()
 
-  setTimeout(drawGraphicOrder, 100)
+  setTimeout(drawGraphicOrder, 300)
 }
 
 drawGraphicOrder()
